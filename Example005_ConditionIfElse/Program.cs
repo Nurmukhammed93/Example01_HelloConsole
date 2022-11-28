c4e75f64c4ca69765beab5d1a36fe3884989c0ff
Nurik <tursvnbaj93@bk.ru>
2022-11-28 14:40:30 +0600

    Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "ару")
+{
+    Console.WriteLine("Ура, это же АРУ");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+    }
+
